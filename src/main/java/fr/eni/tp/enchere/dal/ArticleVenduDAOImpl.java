72fabe5207ec80b78ba17ee60a3470fbeb8a05a7
Louis LESAGE <louis.lesage2025@campus-eni.fr>
2025-07-01 16:43:56 +0200

    en cours d'implementation

--- src/main/java/fr/eni/tp/enchere/dal/ArticleVenduDAOImpl.java
+++ src/main/java/fr/eni/tp/enchere/dal/ArticleVenduDAOImpl.java
@@ -1,19 +1,56 @@
 package fr.eni.tp.enchere.dal;
 
+import com.fasterxml.jackson.databind.util.Named;
 import fr.eni.tp.enchere.bo.ArticleVendu;
+import fr.eni.tp.enchere.bo.Utilisateur;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ArticleVenduDAOImpl implements ArticleVenduDAO {
 
+    private NamedParameterJdbcTemplate jdbc;
+
     private String INSERT = "INSERT INTO ARTICLES_VENDUS(nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie) VALUES(:nom_article, :description, :date_debut_encheres, :date_fin_encheres, :prix_initial, :prix_vente, :no_utilisateur, :no_categorie)";
+    private String SELECT_BY_ID = "SELECT * FROM ARTICLES_VENDUS WHERE no_article = :no_article";
 
-    @Override
-    public void create(ArticleVendu articleVendu) {
 
+    public ArticleVenduDAOImpl(NamedParameterJdbcTemplate jdbc) {
+        this.jdbc = jdbc;
     }
 
     @Override
-    public ArticleVendu readById(int id) {
-        return null;
+    public void create(ArticleVendu articleVendu) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        map.addValue("nom_article", articleVendu.getNomArticle());
+        map.addValue("description", articleVendu.getDescription());
+        map.addValue("date_debut_encheres", articleVendu.getDateDebutEnchere());
+        map.addValue("date_fin_encheres", articleVendu.getDateFinEnchere());
+        map.addValue("prix_initial", articleVendu.getMiseAPrix());
+        map.addValue("prix_vente", articleVendu.getPrixVente());
+        map.addValue("no_utilisateur", articleVendu.getUtilisateur().getNoUtilisateur());
+        map.addValue("no_categorie", articleVendu.getCategorie().getNoCategorie());
+
+        jdbc.update(INSERT, map, keyHolder);
+
+        if(keyHolder.getKey() != null) {
+            articleVendu.setNoArticle(keyHolder.getKey().intValue());
+        }
+    }
+
+    @Override
+    public ArticleVendu readById(int no_article) {
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        map.addValue("no_article", no_article);
+
+        return jdbc.queryForObject(SELECT_BY_ID, map, new ArticleVenduRowMapper());
     }
 
     @Override
@@ -29,5 +66,23 @@ public class ArticleVenduDAOImpl implements ArticleVenduDAO {
     @Override
     public void delete(int id) {
 
+    }
+
+    private class ArticleVenduRowMapper implements RowMapper<ArticleVendu> {
+
+        @Override
+        public ArticleVendu mapRow(ResultSet rs, int rowNum) throws SQLException {
+            final ArticleVendu article = new ArticleVendu();
+            article.setNoArticle(rs.getInt("no_article"));
+            article.setNomArticle(rs.getString("nom_article"));
+            article.setDescription(rs.getString("description"));
+            article.setDateDebutEnchere(rs.getDate("date_debut_encheres"));
+            article.setDateFinEnchere(rs.getDate("date_fin_encheres"));
+            article.setMiseAPrix(rs.getInt("prix_initial"));
+            article.setPrixVente(rs.getInt("prix_vente"));
+//            article.setUtilisateur();
+
+            return article;
+        }
     }
 }
